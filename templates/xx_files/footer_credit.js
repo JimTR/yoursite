54ad1cde70e861f9b2c4b8f97fdf7e74c27afaa7
JimTr <jim.richardson06@btinternet.com>
2014-07-12 16:56:22 +0100

    modified:   README.md 	deleted:    templates/xx_files/36d31ba8f1 	deleted:    templates/xx_files/acp.png 	deleted:    templates/xx_files/close.gif 	deleted:    templates/xx_files/community_forum.png 	deleted:    templates/xx_files/footer_credit.js 	deleted:    templates/xx_files/ga.js 	deleted:    templates/xx_files/general.js 	deleted:    templates/xx_files/in.php 	deleted:    templates/xx_files/js 	deleted:    templates/xx_files/lightbox.js 	deleted:    templates/xx_files/loading.gif 	deleted:    templates/xx_files/moo.ajax.js 	deleted:    templates/xx_files/new_reply.png 	deleted:    templates/xx_files/nr-411.min.js 	deleted:    templates/xx_files/prototype.lite.js 	deleted:    templates/xx_files/rss.gif 	deleted:    templates/xx_files/screen.css

--- templates/xx_files/footer_credit.js
+++
@@ -1,1 +0,0 @@
-document.write('<p><a href="http://www.flickr.com/photos/9619972@N08/3437096285/">Header by Luc De Leeuw</a>.</p>');
--- templates/xx_files/in.php
+++
@@ -1,3 +0,0 @@
-var NO_PINGY=1;(function(){var d=new Date;d.setTime(d.getTime()+86400*1000);document.cookie="no_tracky_66594456=1;expires="+d.toGMTString()+";path=/;";})();
-// exit bad site id
-var o_O
