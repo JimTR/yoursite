435a73f37c9f632d474ba2403034a3028d8269a6
JimTr <jim.richardson06@btinternet.com>
2014-07-13 09:19:48 +0100

    Alters error box.

--- login.php
+++ login.php
@@ -1,13 +1,11 @@
 <?PHP
 
-    require 'includes/master.inc.php'; // do login or not
-//echo printr($_SERVER);
+    require 'includes/master.inc.php'; // load required files
 
     if($Auth->loggedIn()) 
            {
 			   // already logged in default to the main index
-			   $goto = ($site->settings['url']."/index.php");
-			   //die ("logged in");
+			   $goto = ($_SERVER['HTTP_REFERER']);
 			   redirect($goto);
 			   
 			   
@@ -19,49 +17,46 @@
         {
 			//successful login 
 			$goto = $_COOKIE['redirect'];
-            setcookie ("redirect", "", time() - 3600,'/');
+            setcookie ("redirect", "", time() - 3600,'/'); // clear the login cookie
             redirect($goto);
         }
         else
         {
-			$Shit="You Wanker, ";
-            $Error->add($Shit, "We're sorry, you have entered an incorrect username or password. Please try again.");
+			
+            $Error = "We're sorry, you have entered an incorrect username or password. Please try again.";
            
 	   }
     }
 
     // Clean the submitted username before redisplaying it.
     $username = isset($_POST['username']) ? htmlspecialchars($_POST['username']) : '';
-    //echo "here";
+    $users = $database->num_rows("select * from sessions");
+    $job = $_SERVER['HTTP_REFERER'];
+	setcookie('redirect',$job, time() + (60 * 5),'/'); // make sure we go back
     $header = file_get_contents ( $site->settings['url'].'/templates/header.html');
 	$footer = file_get_contents (  $site->settings['url'].'/templates/footer.tmpl');
 	$include = file_get_contents ( $site->settings['url'].'/templates/include.tmpl');
 	$css = $site->settings['url'].'/css/yuiapp.css';
     $css ="<style>".file_get_contents ($css)."</style>";
     $template = new Template;
-	$job = $_SERVER['HTTP_REFERER'];
-	setcookie('redirect',$job, time() + (60 * 5),'/');
-	$users = $database->num_rows("select * from sessions");
 	$template->load($site->settings['url'].'/templates/login.html');
-$template->replace("result"," our database");
-$template->replace("css",$css);
-$template->replace("error",$Error);
-$template->replace("title", "Login");
-$template->replace("header", $header);
-$template->replace("footer", $footer);
-$template->replace("include", $include);
-$template->replace ("path", $site->settings['url']);
-$template->replace("name",$name );
-$template->replace("login","");
-$template->replace("vari",$users);
-//$template->replace("stuff",$stuff);
-$template->replace("datetime", FORMAT_TIME);
-if($site->settings['showphp'] === false)
-{
-//$template->removephp();
-}
+	$template->replace("css",$css);
+	$template->replace("error",$Error);
+	$template->replace("title", "Login");
+	$template->replace("header", $header);
+	$template->replace("footer", $footer);
+	$template->replace("include", $include);
+	$template->replace ("path", $site->settings['url']);
+	$template->replace("name",$name );
+	$template->replace("login","");
+	$template->replace("vari",$users);
+	$template->replace("datetime", FORMAT_TIME);
+		if($site->settings['showphp'] === false)
+			{
+				$template->removephp();
+			}
 
-$template->publish();
+	$template->publish();
 
 ?>
 
