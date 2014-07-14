02cbae44b919bbfb255e054be35295a618b1a627
JimTr <jim.richardson06@btinternet.com>
2014-07-14 02:11:50 +0100

    now uses some validation

--- login.php
+++ login.php
@@ -22,17 +22,20 @@
         }
         else
         {
-			
-            $Error = "We're sorry, you have entered an incorrect username or password. Please try again.";
+			//die ('no user name');
+            $Error = "You have entered an incorrect username/password combination.<br> Please try again. ";
+           
            
 	   }
+		
     }
-
+    
+	
     // Clean the submitted username before redisplaying it.
     $username = isset($_POST['username']) ? htmlspecialchars($_POST['username']) : '';
     $users = $database->num_rows("select * from sessions");
     $job = $_SERVER['HTTP_REFERER'];
-	setcookie('redirect',$job, time() + (60 * 5),'/'); // make sure we go back
+	if (!$_COOKIE['redirect']) {setcookie('redirect',$job, time() + (60 * 5),'/');} // make sure we go back
     $header = file_get_contents ( $site->settings['url'].'/templates/header.html');
 	$footer = file_get_contents (  $site->settings['url'].'/templates/footer.tmpl');
 	$include = file_get_contents ( $site->settings['url'].'/templates/include.tmpl');
@@ -41,7 +44,6 @@
     $template = new Template;
 	$template->load($site->settings['url'].'/templates/login.html');
 	$template->replace("css",$css);
-	$template->replace("error",$Error);
 	$template->replace("title", "Login");
 	$template->replace("header", $header);
 	$template->replace("footer", $footer);
@@ -51,12 +53,16 @@
 	$template->replace("login","");
 	$template->replace("vari",$users);
 	$template->replace("datetime", FORMAT_TIME);
+	//$template->replace("error",$Error);
 		if($site->settings['showphp'] === false)
 			{
 				$template->removephp();
 			}
 
 	$template->publish();
-
+	if ($Error <> '')
+	{
+	 echo '<script type="text/javascript">alertify.alert("'.$Error.'");</script>';
+ }
 ?>
 
